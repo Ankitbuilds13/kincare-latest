2c7b94721b3f8a04419c03b922e542f2baef44f6
Your Name <ankitshrivastava0130@gmail.com>
2026-09-07 15:56:58 +0530

    fix: ignore node_modules and update build

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.kincare.app',
+  appName: 'kincare',
+  webDir: 'dist'
+};
+
+export default config;
